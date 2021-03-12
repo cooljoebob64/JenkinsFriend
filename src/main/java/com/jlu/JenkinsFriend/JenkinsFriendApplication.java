cda054925b77aae9b0a74c329c7f388b45a71eb2
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-12 08:59:09 -0600

    Create initial project files from Spring Initializr

---
+++ src/main/java/com/jlu/JenkinsFriend/JenkinsFriendApplication.java
@@ -0,0 +1,13 @@
+package com.jlu.JenkinsFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlu/JenkinsFriend/JenkinsFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlu.JenkinsFriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
